df2a1dbddb12e5ab33e05c9b323c2975beb4782a
Rafe Taya <rafe.otaya@gmail.com>
2025-02-22 07:34:06 +0200

    Added lesson1 exercise 1

---
+++ lesson1/exercise1.py
@@ -0,0 +1,11 @@
+header = "-" * 20
+header1 = "sf_gw1"
+header2 = "sf_gw2"
+
+sf_gw1 = "172.31.255.1/24"
+sf_gw2 = input(f"Please input 'sf_gw2' IP Address: ")
+
+print()
+print(f"{header1:^20} {header2:^20}")
+print(f"{header:^20} {header:^20}")
+print(f"{sf_gw1:^20} {sf_gw2:^20}")
