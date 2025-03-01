98aed47445833ff9721f670c5ee8461c7c626993
Rafe Taya <rafe.otaya@gmail.com>
2025-03-01 15:14:07 +0200

    Added lesson1 exercise5

---
+++ lesson1/exercise5.py
@@ -0,0 +1,11 @@
+ip_addr = "10.12.17.1"
+mac_addr = "0024.c4e9.48ae"
+print()
+print("String concatenation")
+print(ip_addr + " --> " + mac_addr + "\n") 
+
+print("f-strings")
+print(f"{ip_addr} --> {mac_addr}\n")
+
+print("format() method")
+print("{} --> {}".format(ip_addr, mac_addr))
