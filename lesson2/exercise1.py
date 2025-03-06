4478e744171cc0943468fd36a05f834cea1f3864
Rafe Taya <rafe.otaya@gmail.com>
2025-03-06 14:06:40 +0200

    Added lesson2 exercise2

---
+++ lesson2/exercise1.py
@@ -0,0 +1,25 @@
+ip_addr = "192.168.254."
+
+subnet_prefix = int(input("Enter the subnet prefix length between 25 to 30: "))
+
+host_bits = 32 - subnet_prefix
+
+#each subnet starts at a multiple of this increment starting 
+#from zero
+subnet_increment = 2 ** host_bits
+
+#number of hosts (after taking out subnet network address 
+#and broadcoast addres address)
+number_hosts = subnet_increment - 2
+
+print(f"1. The number of hosts in the subnet: {number_hosts} \n")
+
+print(f"2. The network number of the first two subnets: ")
+print(f"{ip_addr}0")
+print(f"{ip_addr}{subnet_increment} \n")
+
+print(f"3. The first and last host addresses in the first subnet")
+#first host in first subnet
+print(f"{ip_addr}1")
+#second host in first subnet
+print(f"{ip_addr}{subnet_increment-2}")
